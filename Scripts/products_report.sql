852801dd87292015ca4321d8fe74d754507e077a
Mohammad Abo Al-rub <129693613+Mohammad02alrub@users.noreply.github.com>
2026-01-28 19:31:33 +0300

    Add files via upload

---
+++ Scripts/products_report.sql
@@ -0,0 +1,124 @@
+
+/*
+===============================================================================
+View Name: gold.products_report
+
+Description:
+This view creates a product-level performance report based on sales transactions.
+It follows three main steps:
+
+1) Base Query:
+   - Joins the fact_sales table with dim_products.
+   - Selects core transactional and product attributes.
+   - Filters out records with NULL order dates.
+
+2) Product Aggregation:
+   - Aggregates data by product.
+   - Calculates KPIs such as:
+        * Lifespan (in months between first and last sale)
+        * Last sale date
+        * Total orders
+        * Total customers
+        * Total sales and quantity
+        * Average selling price
+
+3) Final Output:
+   - Adds business metrics such as:
+        * Recency in months
+        * Product performance segment (High / Mid / Low)
+        * Average order revenue
+        * Average monthly revenue
+
+The result is an analytics-ready view for product performance monitoring
+and reporting in the Gold layer.
+===============================================================================
+*/
+
+IF OBJECT_ID('gold.products_report', 'V') IS NOT NULL
+    DROP VIEW gold.products_report;
+GO
+
+CREATE VIEW gold.products_report AS
+
+WITH base_query AS (
+-- 1) Base Query: Retrieves core columns from fact_sales and dim_products
+    SELECT
+	    f.order_number,
+        f.order_date,
+		f.customer_key,
+        f.sales_amount,
+        f.quantity,
+        p.product_key,
+        p.product_name,
+        p.category,
+        p.sub_category,
+        p.product_cost
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON f.product_key = p.product_key
+    WHERE order_date IS NOT NULL  -- only consider valid sales dates
+), 
+
+ product_aggregation AS (
+-- 2) Product Aggregations: Summarizes key metrics at the product level
+	SELECT
+		product_key,
+		product_name,
+		category,
+		sub_category,
+		product_cost,
+		DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+		MAX(order_date) AS last_sale_date,
+		COUNT(DISTINCT order_number) AS total_orders,
+		COUNT(DISTINCT customer_key) AS total_customers,
+		SUM(sales_amount) AS total_sales,
+		SUM(quantity) AS total_quantity,
+		ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),1) AS avg_selling_price
+	FROM base_query
+
+	GROUP BY
+		product_key,
+		product_name,
+		category,
+		sub_category,
+		product_cost
+)
+
+
+-- 3) Final Query: Combines all customer results into one output
+SELECT 
+	product_key,
+	product_name,
+	category,
+	sub_category,
+	product_cost,
+	last_sale_date,
+	DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
+	CASE
+		WHEN total_sales > 50000 THEN 'High-Performer'
+		WHEN total_sales >= 10000 THEN 'Mid-Range'
+		ELSE 'Low-Performer'
+	END AS product_segment,
+	lifespan,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_customers,
+	avg_selling_price,
+	-- Average Order Revenue (AOR)
+	CASE 
+		WHEN total_orders = 0 THEN 0
+		ELSE total_sales / total_orders
+	END AS avg_order_revenue,
+
+	-- Average Monthly Revenue
+	CASE
+		WHEN lifespan = 0 THEN total_sales
+		ELSE total_sales / lifespan
+	END AS avg_monthly_revenue
+
+FROM product_aggregation;
+go
+
+SELECT * FROM gold.products_report;
+
